ceeabfd52e74aa2ec8749c651bfa0b2d42a02c6b
Marton Demeter <marton1029@gmail.com>
2017-12-22 18:38:34 -0800

    removed unnecessary promisification

--- src/modules/document.js
+++ src/modules/document.js
@@ -1,22 +1,21 @@
 const fs = require('fs');
 const rp = require('request-promise');
 
-saveDocument = (path,data) => {
-  return new Promise((resolve, reject) => {
-    if(data) {
-      fs.writeFile(path,data,'binary',(error)=>{
-        if(error) reject(error);
-        resolve();
-      });
-    }
-  });
+saveDocument = (path,data,r) => {
+  r = r || 0;
+  if(data) {
+    fs.writeFile(path,data,'binary',(error)=>{
+      if(error && r < 3) saveDocument(path,data,r++);
+    });
+  }
 }
 
 downloadDocument = (jar,url,path) => {
   rp({method:'get',encoding:'binary',jar:jar,url:url
   }).then((data) => {
+    console.log(`Download finished ${path}`);
     saveDocument(path,data);
-  })
+  }).catch((error) => {});
 }
 
 module.exports = { 
