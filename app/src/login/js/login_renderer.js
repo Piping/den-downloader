4b37ab5ea3baf52971400722d7f162378b313487
Marton Demeter <marton1029@gmail.com>
2017-12-25 00:18:16 -0800

    added password length checking

--- app/src/login/js/login_renderer.js
+++ app/src/login/js/login_renderer.js
@@ -4,15 +4,11 @@ const ipcRenderer = require('electron').ipcRenderer;
 window.onload = () => {
   $('#user').on('input', function() {
     if($(this).val() === '') {
-      $(this).removeClass('is-danger');
-      $(this).removeClass('is-primary');
-      $('#user-i').removeClass('fa-warning');
-      $('#user-i').removeClass('fa-check');
+      $(this).removeClass('is-danger is-primary');
+      $('#user-i').removeClass('fa-warning fa-check');
     } else if(/^[a-z0-9]+@usc.edu$/g.test($(this).val())) {
-      $(this).addClass('is-primary');
-      $(this).removeClass('is-danger');
-      $('#user-i').addClass('fa-check');
-      $('#user-i').removeClass('fa-warning');
+      $(this).addClass('is-primary').removeClass('is-danger');
+      $('#user-i').addClass('fa-check').removeClass('fa-warning');
     } else {
       $(this).addClass('is-danger');
       $('#user-i').addClass('fa-warning');
@@ -21,11 +17,14 @@ window.onload = () => {
   
   $('#pass').on('input', function() {
     if($(this).val() === '') {
-      $(this).removeClass('is-primary');
-      $('#pass-i').removeClass('fa-check');
+      $(this).removeClass('is-primary is-danger');
+      $('#pass-i').removeClass('fa-check fa-warning');
+    } else if($(this).val().length > 8 && $(this).val().length < 50){
+      $(this).addClass('is-primary').removeClass('is-danger');
+      $('#pass-i').addClass('fa-check').removeClass('fa-warning');
     } else {
-      $(this).addClass('is-primary');
-      $('#pass-i').addClass('fa-check');
+      $(this).addClass('is-danger').removeClass('is-primary');
+      $('#pass-i').addClass('fa-warning').removeClass('fa-check');
     }
   });
   
