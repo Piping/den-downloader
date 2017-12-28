54062b7cc24dfe4fb7a9e123df1605984db0efe8
Marton Demeter <marton1029@gmail.com>
2017-12-28 02:50:50 -0800

    promisification for feedback

--- app/src/modules/document.js
+++ app/src/modules/document.js
@@ -1,20 +1,24 @@
 const fs = require('fs');
 const rp = require('request-promise');
 
-saveDocument = (path,data,r) => {
-  r = r || 0;
-  if(data) {
-    fs.writeFile(path,data,'binary',(error)=>{
-      if(error && r < 3) saveDocument(path,data,r++);
+saveDocument = (path,data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path,data,'binary',error => {
+      if(error) reject('failure');
+      resolve('success');
     });
-  }
+  });
 }
 
 downloadDocument = (jar,url,path) => {
-  rp({method:'get',encoding:'binary',jar:jar,url:url
-  }).then((data) => {
-    saveDocument(path,data);
-  }).catch((error) => {});
+  return new Promise((resolve, reject) => {
+    rp({method:'get',encoding:'binary',jar:jar,url:url})
+    .then(async data => {
+      return await saveDocument(path,data);
+    })
+    .then(result => resolve(result))
+    .catch(error => reject('failure'));
+  });
 }
 
 module.exports = { 
