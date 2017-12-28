9ebf80dc1332b7a52cd70fd4e601a1edacd55360
Marton Demeter <marton1029@gmail.com>
2017-12-28 02:50:00 -0800

    added downloading state & file watching

--- app/src/content/js/content_renderer.js
+++ app/src/content/js/content_renderer.js
@@ -1,7 +1,5 @@
 window.$ = window.jQuery = require('jquery');
 const ipcRenderer = require('electron').ipcRenderer;
-const crypto = require('crypto');
-var globalDownloadContainer = Array();
 
 window.onload = () => {
   ipcRenderer.on('login-failure', (event, arg) => {
@@ -33,10 +31,25 @@ window.onload = () => {
     $('#container').html('');
     $('#container').append(result);
   });
+  
   var globalCourseArchive = Array();
   var globalOutputDirectory = require('path').join(__dirname,'..','..','content');
+  
+  
+  setUpFilewatcher = () => {
+    globalCourseArchive.forEach((course) => {
+      var d = require('path').join(globalOutputDirectory,course.id,'documents');
+      var v = require('path').join(globalOutputDirectory,course.id,'videos');
+      try {
+        require('fs').watch(d,checkFiles);
+        require('fs').watch(v,checkFiles);
+      } catch(error) {}
+    });
+  }
+
   ipcRenderer.on('courses-list', (event, arg) => {
     globalCourseArchive = arg;
+    setUpFilewatcher();
     $('#container').removeClass('justify-content-center');
     $('#container').addClass('justify-content-start');
     $('#container').html('');
@@ -71,26 +84,15 @@ window.onload = () => {
           link:$(el).attr('download-link'),
           ext:$(el).attr('extension'),
           course:$(el).attr('course'),
+          id:$(el).attr('id'),
           title:$(el).text()
         });
+        setItemDownloading($(el));
       });
-      var hashExisting = crypto.createHash('md5');
-      hashExisting = hashExisting.update(JSON.stringify(globalDownloadContainer));
-      hashExisting = hashExisting.digest('hex');
-      var hashNew = crypto.createHash('md5');
-      hashNew = hashNew.update(JSON.stringify(download_list));
-      hashNew = hashNew.digest('hex');
-      if(hashNew != hashExisting) {
-        $('.download').each((i,el) => {
-          setItemDownloading($(el));
-          $('#download-item-number').text('0');
-        });
-        globalDownloadContainer = download_list;
-        var obj = Object();
-        obj.target = globalOutputDirectory;
-        obj.list = download_list;
-        ipcRenderer.send('download-request',obj);
-      }
+      var obj = Object();
+      obj.target = globalOutputDirectory;
+      obj.list = download_list;
+      ipcRenderer.send('download-request',obj);
     });
     $(panel_header).append(title).append(tags).append(folder_container);
     $(panel).append(panel_header);
@@ -100,28 +102,7 @@ window.onload = () => {
       var checkbox_container = document.createElement('label');
       var checkbox = document.createElement('i');
       $(checkbox).addClass('fa fa-square-o');
-      $(checkbox).click(function(event) {
-        event.stopPropagation();
-        if($(this).hasClass('fa-square-o')) {
-          $(this).removeClass('fa-square-o').addClass('fa-check-square');
-          var container = $(this).parent().parent().next();
-          $(container).find('.sub-section').each((i,el) => {
-            var icon = $(el).find('i').first();
-            if(icon.hasClass('fa-square-o'))
-              $(icon).removeClass('fa-square-o').addClass('fa-check-square');
-          });
-        }
-        else {
-          $(this).removeClass('fa-check-square').addClass('fa-square-o');
-          var container = $(this).parent().parent().next();
-          $(container).find('.sub-section').each((i,el) => {
-            var icon = $(el).find('i').first();
-            if(icon.hasClass('fa-check-square'))
-              $(icon).removeClass('fa-check-square').addClass('fa-square-o');
-          });
-        }
-        updateDownloadCounter();
-      });
+      $(checkbox).click(toggleCheckboxesContainer);
       $(checkbox).css('visibility','hidden');
       $(checkbox_container).append(checkbox);
       var title = `${course.id} - ${course.title} (${course.term})`;
@@ -165,32 +146,9 @@ window.onload = () => {
       $(subsection).attr('download-link',item.link);
       $(subsection).attr('extension',item.ext);
       $(subsection).attr('course',item.course);
-      $(subsection).click(function() {
-      });
-    } else {
-      $(checkbox).click(function(event) {
-        event.stopPropagation();
-        if($(this).hasClass('fa-square-o')) {
-          $(this).removeClass('fa-square-o').addClass('fa-check-square');
-          var container = $(this).parent().parent().next();
-          $(container).find('.sub-section').each((i,el) => {
-            var icon = $(el).find('i').first();
-            if(icon.hasClass('fa-square-o'))
-              icon.removeClass('fa-square-o').addClass('fa-check-square');
-          });
-        }
-        else {
-          $(this).removeClass('fa-check-square').addClass('fa-square-o');
-          var container = $(this).parent().parent().next();
-          $(container).find('.sub-section').each((i,el) => {
-            var icon = $(el).find('i').first();
-            if(icon.hasClass('fa-check-square'))
-              icon.removeClass('fa-check-square').addClass('fa-square-o');
-          });
-        }
-        updateDownloadCounter();
-      });
-    }
+      $(subsection).attr('id',item.id);
+      $(subsection).click(subsectionItemOnClick);
+    } else $(checkbox).click(toggleCheckboxesContainer);
     $(checkbox_container).append(checkbox);
     var name = document.createTextNode(title)
     $(checkbox_container).append(name);
@@ -206,30 +164,50 @@ window.onload = () => {
       var subsection_container = document.createElement('div');
       $(subsection_container).addClass('sub-section material-container');
       $(subsection_container).attr('id',id);
-      $(subsection).click(function() {
-        var container = $(this).next();
-        if($(container).css('display') == 'none') {
-          $(container).css('display','block');
-          $(container).find('.material-container').prev().css('display','block');
-          $(this).find('i').last().removeClass('fa-plus-circle').addClass('fa-minus-circle');
-        } else {
-          $(container).css('display','none');
-          $(container).find('.material-container').prev().css('display','none');
-          $(this).find('i').last().removeClass('fa-minus-circle').addClass('fa-plus-circle');
-        }
-      });
+      $(subsection).click(toggleSubsectionContainer);
     } else checkFileExists(title,item.type,item.course,item.ext,$(subsection));
     $(parent).append(subsection);
     if(container) $(parent).append(subsection_container);
   }
   
   updateDownloadCounter = () => {
-    $('.downloadable-item').removeClass('download bg-primary has-text-white');
-    var elements = $('.downloadable-item').find('i.fa-check-square');
-    $(elements).parent().parent().addClass('download bg-primary has-text-white');
-    var number = elements.length;
-    number = number || 0;
-    $('#download-item-number').text(number);
+    $('#download-item-number').text($('.download').length);
+  }
+  
+  function toggleCheckboxesContainer() {
+    event.stopPropagation();
+    $(this).toggleClass('fa-square-o');
+    $(this).toggleClass('fa-check-square');
+    var container = $(this).parent().parent().next();
+    if($(this).hasClass('fa-square-o')) {
+      $(container).find('i.fa-check-square')
+      .removeClass('fa-check-square')
+      .addClass('fa-square-o');
+    } else {
+      $(container).find('i.fa-square-o')
+      .removeClass('fa-square-o')
+      .addClass('fa-check-square');
+    }
+    $(container).find('i.fa-check-square:not(.material-container)')
+    .parent().parent('.downloadable-item:not(.downloaded,.downloading)')
+    .addClass('download');
+    $(container).find('i.fa-square-o:not(.material-container)')
+    .parent().parent('.downloadable-item:not(.downloaded,.downloading)')
+    .removeClass('download');
+    updateDownloadCounter();
+  }
+  
+  function toggleSubsectionContainer() {
+    var container = $(this).next();
+    if($(container).css('display') == 'none') {
+      $(container).css('display','block');
+      $(container).find('.material-container').prev().css('display','block');
+      $(this).find('i').last().removeClass('fa-plus-circle').addClass('fa-minus-circle');
+    } else {
+      $(container).css('display','none');
+      $(container).find('.material-container').prev().css('display','none');
+      $(this).find('i').last().removeClass('fa-minus-circle').addClass('fa-plus-circle');
+    }
   }
   
   checkFileExists = (title,type,course,extension,element) => {
@@ -238,26 +216,61 @@ window.onload = () => {
     var d = require('path').join(globalOutputDirectory,course,type);
     var f = require('path').join(d,`${title}.${extension}`);
     require('fs').access(f,(error) => {
-      if(error) { clearItemDownloading($(element)); return; }
-      setItemDownloading($(element));
+      if(error) { clearItemDownloaded(element); return; }
+      setItemDownloaded(element);
     });
   }
   
-  setItemDownloading = (item) => {
-    $(item).addClass('bg-primary has-text-white');
-    $(item).removeClass('download').addClass('downloading');
-    $(item).removeClass('downloadable-item');
-    $(item).find('i').first().addClass('is-invisible');
-    $(item).find('i').last().removeClass('is-invisible');
-    $(item).unbind('click');
+  createSpinner = () => {
+    var wrapper = document.createElement('div');
+    $(wrapper).addClass('preloader-wrapper tiny active');
+    var layer = document.createElement('div');
+    $(layer).addClass('spinner-layer spinner-white');
+    var left = document.createElement('div');
+    $(left).addClass('circle-clipper left');
+    var gap = document.createElement('div');
+    $(gap).addClass('gap-patch');
+    var right = document.createElement('div');
+    $(right).addClass('circle-clipper right');
+    var circle = document.createElement('div');
+    $(circle).addClass('circle');
+    $(left).append(circle);
+    $(gap).append(circle);
+    $(right).append(circle);
+    $(layer).append(left).append(gap).append(right);
+    $(wrapper).append(layer);
+    return $(wrapper);
   }
   
-  clearItemDownloading = (item) => {
-    $(item).removeClass('bg-primary has-text-white');
+  setItemDownloaded = (item) => {
+    $(item).removeClass('download downloading downloadable-item');
+    $(item).addClass('downloaded');
+    $(item).find('div').css('display','none');
+    $(item).find('i').first().addClass('is-invisible');
+    $(item).find('i').last().removeClass('is-invisible');
+    updateDownloadCounter();
+  }
+  
+  setItemDownloading = (item) => {
+    $(item).removeClass('download downloadable-item download-failed');
+    $(item).addClass('downloading');
+    $(item).find('i').first().addClass('is-invisible');
+    $(item).append(createSpinner());
+    updateDownloadCounter();
+  }
+  
+  clearItemDownloaded = (item) => {
+    if($(item).hasClass('downloading')) $(item).removeClass('download');
+    if($(item).hasClass('downloaded')) $(item).removeClass('download');
+    $(item).removeClass('downloading downloaded');
     $(item).addClass('downloadable-item');
     $(item).find('i').first().removeClass('is-invisible');
+    if($(item).hasClass('download'))
+      $(item).find('i').first().removeClass('fa-square-o').addClass('fa-check-square');
+    else
+      $(item).find('i').first().removeClass('fa-check-square').addClass('fa-square-o');
     $(item).find('i').last().addClass('is-invisible');
-    $(item).click(() => subsectionNotContainerOnClick($(item)));
+    updateDownloadCounter();
   }
   
   courseContainerOnClick = (sibling) => {
@@ -271,17 +284,30 @@ window.onload = () => {
       $(container).find('.material-container').prev().css('display','none');
       $(sibling).find('i').last().removeClass('fa-minus-circle').addClass('fa-plus-circle');
     }
+  }
+  
+  function subsectionItemOnClick() {
+    var icon = $(this).find('i').first();
+    if(icon.hasClass('fa-square-o')) {
+      $(this).not('.downloading, .downloaded').addClass('download');
+      icon.removeClass('fa-square-o').addClass('fa-check-square');
+    } else {
+      $(this).removeClass('download');
+      icon.removeClass('fa-check-square').addClass('fa-square-o');
+    }
     updateDownloadCounter();
   }
   
-  subsectionNotContainerOnClick = (subsection) => {
-    var icon = $(subsection).find('i').first();
-    if($(icon).hasClass('fa-square-o'))
-      $(icon).removeClass('fa-square-o').addClass('fa-check-square');
-    else
-      $(icon).removeClass('fa-check-square').addClass('fa-square-o');
-    updateDownloadCounter();
+  checkFiles = () => {
+    $('.downloaded, .downloadable-item').each((i,el) => {
+      var title = $(el).text();
+      var course = $(el).attr('course');
+      var type = $(el).attr('type');
+      var ext = $(el).attr('extension');
+      checkFileExists(title,type,course,ext,el);
+    });
   }
+  
   
   outputDirectoryOnClick = () => {
     const dialog = require('electron').remote.dialog;
@@ -289,15 +315,27 @@ window.onload = () => {
       properties: ['openDirectory','createDirectory']
     }, (paths) => {
       globalOutputDirectory = paths.toString();
-      $('.downloading, .downloadable-item').each((i,el) => {
-        var title = $(el).text();
-        var course = $(el).attr('course');
-        var type = $(el).attr('type');
-        var ext = $(el).attr('extension');
-        checkFileExists(title,type,course,ext,$(el));
-      })
+      $('#download-item-number').text('0');
+      checkFiles();
+      setUpFilewatcher();
     });
   }
+  
+  setItemFailed = (item) => {
+    $(item).removeClass('download downloading downloaded');
+    $(item).addClass('downloadable-item download-failed');
+    $(item).find('div').css('display','none');
+    $(item).find('i').first().removeClass('is-invisible');
+    $(item).find('i').last().addClass('is-invisible');
+    updateDownloadCounter();
+  }
+  
+  ipcRenderer.on('download-finished', (event, arg) => {
+    if(arg.result == 'success')
+      setItemDownloaded($(`#${arg.id}`));
+    else
+      setItemFailed($(`#${arg.id}`));
+  });
   
   ipcRenderer.on('content-list', (event, arg) => {
     var parent = $(`#material-${arg.index}-container`);
@@ -306,12 +344,13 @@ window.onload = () => {
     if(arg.content.document.length) {
       var id = `documents-${arg.index}-container`;
       createSubsection(parent,'Documents',1,true,id);
-      arg.content.document.forEach((item) => {
+      arg.content.document.forEach((item,index) => {
         var i = {
           type:'doc',
           link:item.download_link,
           ext:item.extension,
-          course:globalCourseArchive[arg.index].id
+          course:globalCourseArchive[arg.index].id,
+          id:`course-${arg.index}-document-item-${index}`
         };
         createSubsection($(`#${id}`),item.title,2,null,null,i);
       });
@@ -319,12 +358,13 @@ window.onload = () => {
     if(arg.content.video.length) {
       var id = `videos-${arg.index}-container`;
       createSubsection(parent,'Videos',1,true,id);
-      arg.content.video.forEach((item) => {
+      arg.content.video.forEach((item,index) => {
         var i = {
           type:'vid',
           link:item.download_link,
           ext:'ts',
-          course:globalCourseArchive[arg.index].id
+          course:globalCourseArchive[arg.index].id,
+          id:`course-${arg.index}-video-item-${index}`
         };
         createSubsection($(`#${id}`),item.title,2,null,null,i);
       });
