2da71490bf8ecc672497cdee098f89cb544e2906
Marton Demeter <marton1029@gmail.com>
2017-12-28 22:21:38 -0800

    moved downloads to external node process

---
+++ app/src/external/downloadrequest.js
@@ -0,0 +1,25 @@
+const doc = require('../modules/document');
+const util = require('../modules/util');
+const video = require('../modules/video');
+const rp = require('request-promise');
+
+var jar_c = rp.jar();
+
+jar_c._jar.cookies = process.argv[2];
+var obj = JSON.parse(process.argv[3]);
+
+util.generateOutputDirectory(obj.target);
+obj.list.forEach((item) => {
+  if(item.type === 'doc') {
+    var out = util.generateOutputDirectory(obj.target,'doc',item.course);
+    doc.downloadDocument(jar_c,item.link,`${out}/${item.title}.${item.ext}`)
+    .then(result => process.send(JSON.stringify({result:result,id:item.id})))
+    .catch(result => process.send(JSON.stringify({result:result,id:item.id})));
+  } if(item.type === 'vid') {
+    var out = util.generateOutputDirectory(obj.target,'vid',item.course);
+    item.title = item.title.replace(/[\s/]/g,'');
+    video.downloadVideo(item.link,`${out}/${item.title}.${item.ext}`,item.id)
+    .then(result => process.send(JSON.stringify({result:result,id:item.id})))
+    .catch(result => process.send(JSON.stringify({result:result,id:item.id})));
+  }
+});
