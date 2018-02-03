f6c1ee3c387b48d55957d1660dd7e6b6bc680504
Marton Demeter <marton1029@gmail.com>
2018-02-03 13:54:56 -0800

    changed output directory

--- src/external/downloadrequest.js
+++ src/external/downloadrequest.js
@@ -8,15 +8,17 @@ var jar_c = rp.jar();
 jar_c._jar.cookies = process.argv[2];
 var obj = JSON.parse(process.argv[3]);
 
-util.generateOutputDirectory(obj.target);
+// util.generateOutputDirectory(obj.target);
 obj.list.forEach((item) => {
   if(item.type === 'doc') {
-    var out = util.generateOutputDirectory(obj.target,'doc',item.course);
+    // var out = util.generateOutputDirectory(obj.target,'doc',item.course);
+    var out = obj.target;
     doc.downloadDocument(jar_c,item.link,`${out}/${item.title}.${item.ext}`)
     .then(result => process.send(JSON.stringify({result:result,id:item.id})))
     .catch(result => process.send(JSON.stringify({result:result,id:item.id})));
   } if(item.type === 'vid') {
-    var out = util.generateOutputDirectory(obj.target,'vid',item.course);
+    // var out = util.generateOutputDirectory(obj.target,'vid',item.course);
+    var out = obj.target;
     item.title = item.title.replace(/[\s/]/g,'');
     video.downloadVideo(item.link,`${out}/${item.title}.${item.ext}`,item.id)
     .then(result => process.send(JSON.stringify({result:result,id:item.id})))
